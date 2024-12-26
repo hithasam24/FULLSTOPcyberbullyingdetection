58590fd846b950cead89d39831625e2bdc0510d1
hithasam24 <hitha240605@gmail.com>
2024-12-27 01:11:39 +0530

    edites2

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import pickle
-
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
@@ -12,15 +10,29 @@ with open("stopwords.txt", "r") as file:
 vectorizer = TfidfVectorizer(stop_words=stopwords, lowercase=True, vocabulary=pickle.load(open("tfidfvectoizer.pkl", "rb")))
 model = pickle.load(open("LinearSVCTuned.pkl", 'rb'))
 
+# Define a function to map predictions to categories
+def get_bullying_category(prediction):
+    if prediction == 0:
+        return "Not Bullying"
+    elif prediction == 1:
+        return "Bullying due to Ethnicity"
+    elif prediction == 2:
+        return "Bullying due to Age"
+    elif prediction == 3:
+        return "Bullying due to Religion"
+    else:
+        return "Other Bullying"
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    prediction = None
+    category = None
     if request.method == 'POST':
         user_input = request.form['text']
-        transformed_input = vectorizer.fit_transform([user_input])
+        transformed_input = vectorizer.transform([user_input])  # Use transform instead of fit_transform
         prediction = model.predict(transformed_input)[0]
+        category = get_bullying_category(prediction)  # Get the bullying category based on prediction
     
-    return render_template('index.html', prediction=prediction)
+    return render_template('index.html', category=category)
 
 if __name__ == '__main__':
     app.run(debug=True)
